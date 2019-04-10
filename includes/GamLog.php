0855e5749f2fd5f0fbfc9e12c74350b4a81aaf53
blasb <blasbutera69@gmail.com>
2019-04-10 11:32:40 -0300

    updated gitignore

--- includes/GamLog.php
+++ includes/GamLog.php
@@ -3,7 +3,15 @@
 //Clase utilizada para implementar un log
 class GamLog {
     public static function write($text){
-        file_put_contents( __DIR__ . "/logb.log", "[" . date("m/d/Y H:i:s") . "]: " . $text . "\n", FILE_APPEND | LOCK_EX);
-        if(!file_exists(__DIR__ . "/logb.log")) chmod(__DIR__ . "/logb.log", 0666);
+
+        $path = __DIR__ . "/logb.log";
+
+        if(!file_exists($path)){
+            $file = fopen($path, "w");
+            fclose($file);
+            chmod($path, 0666);
+        }
+
+        file_put_contents( $path, "[" . date("m/d/Y H:i:s") . "]: " . $text . "\n", FILE_APPEND | LOCK_EX);
     }
 }
