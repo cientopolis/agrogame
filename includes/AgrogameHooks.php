e53ed94a08afbe5b265067de8cbf21d775ef7597
Blas <blasbutera69@gmail.com>
2019-10-17 16:10:45 -0300

    nueva tabla que guarda eventos

--- includes/AgrogameHooks.php
+++ includes/AgrogameHooks.php
@@ -2,8 +2,29 @@
 
 class AgrogameHooks {
 
+	//Carga js,css en cada reload
     public static function onBeforePageDisplay( OutputPage $out, Skin $skin ) {
 		$out->addModules( 'ext.agrogame' );
+	}
+
+	public static function onLoadExtensionSchemaUpdates( $updater ) {
+		$patchPath = __DIR__ . '/../sql/';
+
+		$updater->addExtensionTable( 'ag_last_events', $patchPath . 'create-table--patch-agrogame.sql' );
+	}
+
+	public static function onUserLoginComplete( User &$user, &$inject_html, $direct ) {
+		$dbw = wfGetDB( DB_MASTER );
+		$dbw->startAtomic( __METHOD__ );
+		$dbw->insert( 'ag_last_events', [ 'ag_event' => 'login', 'ag_timestamp' => date("Y-m-d H:i:s")], __METHOD__ );
+		$dbw->endAtomic( __METHOD__ );
+	}
+
+	public static function onPageContentSaveComplete( $wikiPage, $user, $mainContent, $summaryText, $isMinor, $isWatch, $section, &$flags, $revision, $status, $originalRevId, $undidRevId ){
+		$dbw = wfGetDB( DB_MASTER );
+		$dbw->startAtomic( __METHOD__ );
+		$dbw->insert( 'ag_last_events', [ 'ag_event' => 'save_page', 'ag_timestamp' => date("Y-m-d H:i:s")], __METHOD__ );
+		$dbw->endAtomic( __METHOD__ );
 	}
 
 }
---
+++ sql/create-table--patch-agrogame.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS /*_*/ag_last_events (
+  `ag_event` varchar(255) NOT NULL,
+  `ag_timestamp` TIMESTAMP NOT NULL
+)
