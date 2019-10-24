a1276c32c0acc4e3b742f13f32fc59bdf0850fec
Blas <blasbutera69@gmail.com>
2019-10-24 11:52:28 -0300

    arreglado cartel se muestra en paginas sin votos

--- includes/ApiAVGPage.php
+++ includes/ApiAVGPage.php
@@ -3,11 +3,19 @@
 
 class ApiAVGPage extends ApiBase {
 	public function execute() {
+        global $wgRPRatingAllowedNamespaces;
+        global $wgRPRatingPageBlacklist;
         $params = $this->extractRequestParams();
         if(!isset($params["pagetitle"])){
             $this->dieWithError(ApiMessage::create("Se necesita el parametro title"));
         }
         $title = Title::newFromText( $params['pagetitle'] );
+        if(in_array(str_replace(' ', '_', $title),$wgRPRatingPageBlacklist)){
+           $this->dieWithError(ApiMessage::create("Esta pagina no puede ser votada"));
+        }
+        if(!in_array($title->getNamespace(),$wgRPRatingAllowedNamespaces)){
+            $this->dieWithError(ApiMessage::create("Esta pagina no puede ser votada"));
+        }
         //SELECT `rv_page_id`, avg(`rv_answer`) FROM ratepage_vote WHERE rv_page_id = idpag GROUP BY `rv_page_id`
         $dbr = wfGetDB( DB_REPLICA );
 		$res = $dbr->select(
@@ -17,8 +25,14 @@ class ApiAVGPage extends ApiBase {
             __METHOD__,
             array("GROUP BY" => "rv_page_id")
         );
-        if ($res->numRows() == 0){
-            $this->dieWithError(ApiMessage::create("No existe pagina"));
+        if($res->numRows() == 0){ //página aún no se ha votado.
+            $x = "false";
+            if(!in_array($title->getNamespace(),$wgRPRatingAllowedNamespaces)){
+                $x = "true";
+            }
+            $this->getResult()->addValue(null, "title", $title);
+            //$this->getResult()->addValue(null, "page_id", $res->current()->rv_page_id); no exponer ID de la pag.
+            $this->getResult()->addValue(null, "avg",0);
         }else{
             $this->getResult()->addValue(null, "title", $title);
             //$this->getResult()->addValue(null, "page_id", $res->current()->rv_page_id); no exponer ID de la pag.
