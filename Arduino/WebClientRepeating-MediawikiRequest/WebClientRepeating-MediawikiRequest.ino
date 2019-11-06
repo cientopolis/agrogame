2d79a264664a7383bc6ff8f35583953e2fd6e64e
Blas <blasbutera69@gmail.com>
2019-11-05 21:39:18 -0300

    Request json repeating working

--- Arduino/WebClientRepeating-MediawikiRequest/WebClientRepeating-MediawikiRequest.ino
+++ Arduino/WebClientRepeating-MediawikiRequest/WebClientRepeating-MediawikiRequest.ino
@@ -32,12 +32,6 @@
 #include <ArduinoJson.h>
 #define NLEDS 8
 
-#include <FastLED.h>
-#define BRIGTHNESS 20//0..255
-#define DATA_PIN 6
-#define CLOCK_PIN 13
-CRGB leds[NLEDS];
-
 // assign a MAC address for the ethernet controller.
 // fill in your address here:
 byte mac[] = {
@@ -54,14 +48,11 @@ EthernetClient client;
 IPAddress server(192, 168, 0, 8);
 
 //SETUP PARA EL JSON
-  const size_t capacity = 5*JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(6) + 220;
-  DynamicJsonDocument doc(capacity);
+const size_t capacity = JSON_ARRAY_SIZE(10) + JSON_OBJECT_SIZE(2) + 40;
+DynamicJsonDocument doc(capacity);
 
 unsigned long lastConnectionTime = 0;           // last time you connected to the server, in milliseconds
 const unsigned long postingInterval = 10*1000;  // delay between updates, in milliseconds
-
-/*Setup para el manejo de páginas. Posibilidad hasta 50 paginas.*/
-int lastcalif[NLEDS];
 
 void setup() {
 
@@ -95,75 +86,55 @@ void setup() {
   }
   // give the Ethernet shield a second to initialize:
   delay(1000);
-
-  //Setup paginas
-  for(int i=0; i<NLEDS; i++){
-    lastcalif[i] = -1;
-  }
-
-  //Setup leds
-  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NLEDS);
-  FastLED.setBrightness(BRIGTHNESS);
 }
 
+const char* latest = "20191030142118";
 void loop() {
   
   // if there's incoming data from the net connection.
   // send it out the serial port.  This is for debugging
   // purposes only:
+  //Serial.println("Comienza loop...");
   if (client.available()) {
-    
-    deserializeJson(doc, client);
-
-    for(int i=0; i<NLEDS; i++){
-      leds[i] = CRGB::Black;
-    }
-    FastLED.show();
-
-    /*Deserealiza por cada página*/
-    int n = doc["n"];
-
-    Serial.println(n);
-    
-    //Mapea las páginas a los leds
-    for(int i=0; i<n; i++){
-     leds[i] = CRGB::Blue;
-    }
-    FastLED.show();
-    
-    for(int i=0; i<n; i++){
-      JsonObject page = doc[(String)i];
-      const char* page_id = page["id"];
-      const char* page_last_answer = page["last_answer"];
-      const char* page_avg_stars = page["avg_stars"];
-      /*Si la página no existía festeja*/
-      if(lastcalif[i] != atoi(page_last_answer)){
-        celebrate(i, atoi(page_last_answer));
-        lastcalif[i] = atoi(page_last_answer);
-      }
-    }
-    
+        DeserializationError error = deserializeJson(doc, client);
+        if (error) {
+          Serial.print(F("deserializeJson() failed: "));
+          Serial.println(error.c_str());
+          return;
+        }else{
+          JsonArray events = doc["events"];
+          latest = doc["latest"];
+          for (auto event : events) {
+            Serial.println(event.as<int>());
+          }
+          Serial.println(latest);
+        }
   }
 
   // if ten seconds have passed since your last connection,
   // then connect again and send data:
   if (millis() - lastConnectionTime > postingInterval) {
-    httpRequest();
+    //Serial.println("10 sgs pasaron de la última conexión.");
+    httpRequest(latest);
+  }else{
+    //Serial.println("10 NO sgs pasaron de la última conexión.");
   }
 
 }
 
 // this method makes a HTTP connection to the server:
-void httpRequest() {
+void httpRequest(const char* latest) {
+  //Serial.println("Dentro de httpRequest...");
   // close any connection before send a new request.
   // This will free the socket on the WiFi shield
   client.stop();
 
   // if there's a successful connection:
   if (client.connect(server, 80)) {
-    Serial.println("connecting...");
+    //Serial.println("connecting...");
     // send the HTTP GET request:
-    client.println("GET /mediawiki-1.33.0/api.php?action=agroknowledge&format=json");
+    client.print("GET /mediawiki-1.31.5(actual)/api.php?action=lastevents&format=json&from=");
+    client.println(latest);
     client.println("Host: www.arduino.cc");
     client.println("User-Agent: arduino-ethernet");
     client.println("Connection: close");
@@ -173,30 +144,8 @@ void httpRequest() {
     lastConnectionTime = millis();
   } else {
     // if you couldn't make a connection:
-    Serial.println("connection failed");
+    //Serial.println("connection failed");
   }
+  //Serial.println("Sale de httpRequest...");
 
 }
-
-void celebrate(int nled, int vote){
-  CRGB tmp = leds[nled];
-  for(int i=0; i<vote; i++){
-    leds[nled] = CRGB::Green;
-    FastLED.show();
-    delay(500);
-    leds[nled] = CRGB::Black;
-    FastLED.show();
-    delay(500);  
-  }
-  leds[nled] = tmp;
-  FastLED.show();
-  
-}
-
-/*void printCalif(){
-  for(int i=0; i<NLEDS;i++){
-    Serial.print(" ");
-    Serial.print(lastcalif[i]);
-  }
-  Serial.println(".");
-}*/
--- includes/ApiLastEvents.php
+++ includes/ApiLastEvents.php
@@ -8,18 +8,36 @@ class ApiLastEvents extends ApiBase {
             $this->dieWithError(ApiMessage::create("Se necesita el parametro from"));
         }
         $from = $params['from'];
-        //SELECT * FROM `ag_last_events` WHERE ag_timestamp > '$from'
-        $dbr = wfGetDB( DB_REPLICA );
-        $res = $dbr->query("SELECT * FROM `ag_last_events` WHERE ag_timestamp > '$from'");
         $events = [];
         $latest = $from;
-        foreach ($res as $row) {
-            ($row->ag_timestamp > $latest)?$latest=$row->ag_timestamp:$latest=$latest;
-            array_push($events, $row->ag_event);
+        if($from == "00000000000000"){
+            $latest = gmdate("YmdHis");
+        }else{
+            $dbr = wfGetDB( DB_REPLICA );
+            $res = $dbr->query("SELECT * FROM `ag_last_events` WHERE ag_timestamp > '$from'");
+            foreach ($res as $row) {
+                if(preg_replace("/[^0-9]/", "",$row->ag_timestamp) > $latest)
+                    $latest=$row->ag_timestamp;
+                array_push($events, $row->ag_event);
+            }
+            $latest = preg_replace("/[^0-9]/", "",$latest);
+            $f = function($e){
+                switch ($e) {
+                    case 'login':
+                        return 0;
+                        break;
+                    case 'save_page':
+                        return 1;
+                        break;
+                    default:
+                        $this->dieWithError(ApiMessage::create("No se pudo decodificar el evento de la BD."));
+                        break;
+                }
+            };
+            $events = array_map($f, $events);
         }
         $this->getResult()->addValue(null, "events", $events);
         $this->getResult()->addValue(null, "latest", $latest);
-        //$this->getResult()->addValue(null, "time", date($from));        
     }
     public function getAllowedParams() {
 		return [
